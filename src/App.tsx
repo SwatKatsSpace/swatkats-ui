0b85622518839b8a0dda03fde40be1bcc768732b
Ayush Shukla <ashukla@groupon.com>
2018-11-10 16:40:52 +0530

    adding-article-list-component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import { BaseContainer } from "./app/containers/BaseContainer";
 import { SignupContainer } from "./app/containers/SignupContainer";
+import { ArticleListContainer } from "./app/containers/ArticleListContainer";
+
 
 const AppComponent: SFC = () => (
   <Router basename="/swatkats">
     <Fragment>
       <Route exact path="/" component={BaseContainer} />
       <Route exact path="/signup" component={SignupContainer} />
+      <Route exact path="/list" component={ArticleListContainer} />
     </Fragment>
   </Router>
 );
---
+++ src/app/components/ArticleListComponent.tsx
@@ -0,0 +1,93 @@
+import React, { SFC, Fragment } from "react";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+
+export const ArticleListComponent: SFC<{}> = () => {
+  return (
+    <Fragment>
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            image="https://www.artistsnetwork.com/wp-content/uploads/importedmedia/Drawing-People-Head-proportions-300x177.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+            </Typography>
+            <Typography component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      <br />
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            image="https://www.artistsnetwork.com/wp-content/uploads/importedmedia/Drawing-People-Head-proportions-300x177.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+            </Typography>
+            <Typography component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+      <br />
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            image="https://www.artistsnetwork.com/wp-content/uploads/importedmedia/Drawing-People-Head-proportions-300x177.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+            </Typography>
+            <Typography component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </Fragment>
+  );
+};
